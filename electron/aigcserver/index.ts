b6671b6c307a8cacf3eff08f98c6e085a14b94f2
ModStart <modstart@163.com>
2025-06-23 20:18:13 +0800

    优化：CosyVoice模型调用方式

--- electron/aigcserver/index.ts
+++ electron/aigcserver/index.ts
@@ -1,6 +1,3 @@
-import {ServerCosyvoice} from "./server-cosyvoice";
-import {ServerCosyvoice205b} from "./server-cosyvoice2-0.5b";
-import {ServerCosyvoice2300mInstruct} from "./server-cosyvoice2-300m-instruct";
 import {EasyServer} from "./EasyServer";
 import {ServerLive} from "./server-live";
 
@@ -8,7 +5,4 @@ import {ServerLive} from "./server-live";
 export const AigcServer = {
     'EasyServer': EasyServer,
     'server-live': ServerLive,
-    'server-cosyvoice': ServerCosyvoice,
-    'server-cosyvoice2-0.5b': ServerCosyvoice205b,
-    'server-cosyvoice2-300m-instruct': ServerCosyvoice2300mInstruct,
 }
