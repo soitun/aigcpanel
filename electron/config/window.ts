c6574bddaae0735779011413e537b900581aba93
ModStart <modstart@163.com>
2025-07-12 14:30:35 +0800

    优化：启动更新检测逻辑

--- electron/config/window.ts
+++ electron/config/window.ts
@@ -1,9 +1,9 @@
 export const WindowConfig = {
     alwaysOpenDevTools: true,
-    minWidth: 1100,
-    minHeight: 680,
-    initWidth: 1100,
-    initHeight: 680,
+    minWidth: 1200,
+    minHeight: 700,
+    initWidth: 1200,
+    initHeight: 700,
     aboutWidth: 500,
     aboutHeight: 400,
     feedbackWidth: 600,
