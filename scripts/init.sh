dc2c1d00ec3415a593ab11df83d22d77ba1d02b4
ModStart <modstart@163.com>
2025-10-27 16:11:15 +0800

    feat: remove buildin binary

--- scripts/init.sh
+++ scripts/init.sh
@@ -15,33 +15,33 @@ else
     cd ..
 fi
 
-#rm -rfv electron/resources/extra/osx-arm64
-#mkdir -p electron/resources/extra/osx-arm64
+rm -rfv electron/resources/extra/osx-arm64
+mkdir -p electron/resources/extra/osx-arm64
 #cp -a share-binary/osx-arm64/scrcpy electron/resources/extra/osx-arm64/scrcpy
 #cp -a share-binary/osx-arm64/ffmpeg electron/resources/extra/osx-arm64/ffmpeg
 #cp -a share-binary/osx-arm64/ffprobe electron/resources/extra/osx-arm64/ffprobe
 #
-#rm -rfv electron/resources/extra/osx-x86
-#mkdir -p electron/resources/extra/osx-x86
+rm -rfv electron/resources/extra/osx-x86
+mkdir -p electron/resources/extra/osx-x86
 #cp -a share-binary/osx-x86/ffmpeg electron/resources/extra/osx-x86/ffmpeg
 #cp -a share-binary/osx-x86/ffprobe electron/resources/extra/osx-x86/ffprobe
 
-#rm -rfv electron/resources/extra/linux-arm64
-#mkdir -p electron/resources/extra/linux-arm64
+rm -rfv electron/resources/extra/linux-arm64
+mkdir -p electron/resources/extra/linux-arm64
 #cp -a share-binary/linux-arm64/scrcpy electron/resources/extra/linux-arm64/scrcpy
 #cp -a share-binary/linux-arm64/ffmpeg electron/resources/extra/linux-arm64/ffmpeg
 #cp -a share-binary/linux-arm64/ffprobe electron/resources/extra/linux-arm64/ffprobe
 
-#rm -rfv electron/resources/extra/linux-x86
-#mkdir -p electron/resources/extra/linux-x86
+rm -rfv electron/resources/extra/linux-x86
+mkdir -p electron/resources/extra/linux-x86
 #cp -a share-binary/linux-x86/scrcpy electron/resources/extra/linux-x86/scrcpy
 #cp -a share-binary/linux-x86/ffmpeg electron/resources/extra/linux-x86/ffmpeg
 #cp -a share-binary/linux-x86/ffprobe electron/resources/extra/linux-x86/ffprobe
 #
-#rm -rfv electron/resources/extra/win-x86
-#mkdir -p electron/resources/extra/win-x86
+rm -rfv electron/resources/extra/win-x86
+mkdir -p electron/resources/extra/win-x86
 #cp -a share-binary/win-x86/ScreenCapture.exe electron/resources/extra/win-x86/ScreenCapture.exe
 #cp -a share-binary/win-x86/ffmpeg.exe electron/resources/extra/win-x86/ffmpeg.exe
 #cp -a share-binary/win-x86/ffprobe.exe electron/resources/extra/win-x86/ffprobe
 #
-#ls -R electron/resources/extra
+ls -R electron/resources/extra
